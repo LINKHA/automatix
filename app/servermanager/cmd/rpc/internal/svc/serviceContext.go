c518d9ceca9d0aaa34a6ebd749ff5767b3336e6a
LINKH <421698843@qq.com>
2023-11-06 23:15:38 +0800

    fix servermanager model

--- app/servermanager/cmd/rpc/internal/svc/serviceContext.go
+++ app/servermanager/cmd/rpc/internal/svc/serviceContext.go
@@ -1,9 +1,14 @@
 package svc
 
-import "looklook/app/servermanager/cmd/rpc/internal/config"
+import (
+	"looklook/app/servermanager/cmd/rpc/internal/config"
+	"looklook/app/servermanager/model"
+)
 
 type ServiceContext struct {
 	Config config.Config
+
+	ServerModel model.ServerModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
