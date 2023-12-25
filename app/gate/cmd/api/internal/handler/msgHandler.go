04830642adb9242b5f5d50abdcf0286c666339bd
linkh <421698843@qq.com>
2023-12-26 00:01:23 +0800

    update msg handler

--- app/gate/cmd/api/internal/handler/msgHandler.go
+++ app/gate/cmd/api/internal/handler/msgHandler.go
@@ -2,37 +2,62 @@ package handler
 
 import (
 	"automatix/app/gate/cmd/api/internal/logic"
-	"automatix/common/net/zlog"
-	"fmt"
+	"automatix/common/net/zconf"
+	"automatix/common/net/ziface"
 )
 
 type MsgHandler struct {
-	Apis            map[uint32]logic.IGrpcConnection
-	GrpcTaskQueue   chan GrpcMessage
-	ClientTaskQueue chan ClientMessage
+	GrpcConnManager   *logic.GrpcConnManager
+	ClientConnManager *logic.ClientConnManager
+
+	grpcTaskQueue   []chan GrpcMessage
+	clientTaskQueue []chan ClientMessage
 }
 
 func NewMsgHandler() *MsgHandler {
+	grpcConnManager := logic.NewGrpcConnManager()
+	clientConnManager := logic.NewClientConnManager()
+
 	return &MsgHandler{
-		Apis:            make(map[uint32]logic.IGrpcConnection),
-		GrpcTaskQueue:   make(chan GrpcMessage, 10),
-		ClientTaskQueue: make(chan ClientMessage, 10),
+		GrpcConnManager:   grpcConnManager,
+		ClientConnManager: clientConnManager,
+		grpcTaskQueue:     make(chan GrpcMessage, 10),
+		clientTaskQueue:   make(chan ClientMessage, 10),
 	}
 }
 
-func (mh *MsgHandler) AddConn(msgID uint32, grpcConn logic.IGrpcConnection) {
-	// 1. Check whether the current API processing method bound to the msgID already exists
-	// (判断当前msg绑定的API处理方法是否已经存在)
-	if _, ok := mh.Apis[msgID]; ok {
-		msgErr := fmt.Sprintf("repeated api , msgID = %+v\n", msgID)
-		panic(msgErr)
-	}
-	// 2. Add the binding relationship between msg and API
-	// (添加msg与api的绑定关系)
-	mh.Apis[msgID] = grpcConn
-	zlog.Ins().InfoF("Add Router msgID = %d", msgID)
+func (s *MsgHandler) HandleGrpc(msg GrpcMessage) {
+	s.grpcTaskQueue <- msg
 }
 
-func (mh *MsgHandler) Handle() {
+func (s *MsgHandler) HandleClient(msg ClientMessage) {
+	s.clientTaskQueue <- msg
+}
 
+func (s *MsgHandler) StartOneWorker(workerID int, taskQueue chan ziface.IRequest) {
+	// Continuously wait for messages in the queue
+	// (不断地等待队列中的消息)
+	for {
+		select {
+		// If there is a message, take out the Request from the queue and execute the bound business method
+		// (有消息则取出队列的Request，并执行绑定的业务方法)
+		case request := <-taskQueue:
+
+			switch req := request.(type) {
+
+			case ziface.IFuncRequest:
+				// Internal function call request (内部函数调用request)
+
+				mh.doFuncHandler(req, workerID)
+
+			case ziface.IRequest: // Client message request
+
+				if !zconf.GlobalObject.RouterSlicesMode {
+					mh.doMsgHandler(req, workerID)
+				} else if zconf.GlobalObject.RouterSlicesMode {
+					mh.doMsgHandlerSlices(req, workerID)
+				}
+			}
+		}
+	}
 }
--- app/gate/cmd/api/internal/svc/serviceContext.go
+++ app/gate/cmd/api/internal/svc/serviceContext.go
@@ -2,38 +2,34 @@ package svc
 
 import (
 	"automatix/app/gate/cmd/api/internal/config"
-	"automatix/app/gate/cmd/api/internal/logic"
+	"automatix/app/gate/cmd/api/internal/handler"
 	"automatix/app/rolemanager/cmd/rpc/rolemanager"
 	"automatix/app/roommanager/cmd/rpc/roommanager"
-	"automatix/common/utils"
 
 	"github.com/bwmarrin/snowflake"
 	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
-	Config            config.Config
-	RoleManagerRpc    rolemanager.Rolemanager
-	RoomManagerRpc    roommanager.Roommanager
-	Snowflake         *snowflake.Node
-	StreamManager     *utils.ShardLockMaps
-	ClientConnManager *logic.ClientConnManager
+	Config         config.Config
+	RoleManagerRpc rolemanager.Rolemanager
+	RoomManagerRpc roommanager.Roommanager
+	Snowflake      *snowflake.Node
+	MsgHandler     *handler.MsgHandler
 }
 
 var ServiceContextObj *ServiceContext
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	snowflake, _ := snowflake.NewNode(c.Id)
-	streamManager := utils.NewShardLockMaps()
-	clientConnMgr := logic.NewClientConnManager()
+	msgHandler := handler.NewMsgHandler()
 
 	ServiceContextObj = &ServiceContext{
-		Config:            c,
-		RoleManagerRpc:    rolemanager.NewRolemanager(zrpc.MustNewClient(c.RoleManagerRpcConf)),
-		RoomManagerRpc:    roommanager.NewRoommanager(zrpc.MustNewClient(c.RoomManagerRpcConf)),
-		Snowflake:         snowflake,
-		StreamManager:     &streamManager,
-		ClientConnManager: clientConnMgr,
+		Config:         c,
+		RoleManagerRpc: rolemanager.NewRolemanager(zrpc.MustNewClient(c.RoleManagerRpcConf)),
+		RoomManagerRpc: roommanager.NewRoommanager(zrpc.MustNewClient(c.RoomManagerRpcConf)),
+		Snowflake:      snowflake,
+		MsgHandler:     msgHandler,
 	}
 	return ServiceContextObj
 }
